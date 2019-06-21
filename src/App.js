f3793fa4a54160ed3ef7bdd1ce7fe040980c68c9
radinio <oliver.radini@conted.ox.ac.uk>
2019-06-21 21:49:38 +0100

    reworked to functional component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,50 @@
 import React , {Component} from 'react';
 import './App.css';
 
+const Row = ({ onDelete, text }) => (
+  <tr className="row" key={Math.random()}>
+    <td className="hi">{text}</td>
+    <td>
+    <button className="buttons minus" onClick={onDelete}>
+      <span>-</span>
+    </button>
+   </td>
+  </tr>
+);
 
-class MyTable extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      table: [], 
-    }; 
-  }
-  addRow = () => {
-    const {table} = this.state,
-      tableLength = table.length;
+const MyTable = () => {
+  const [rows, setRows] = React.useState([]);
 
-    table.push(tableLength + 1);
+  const addRow = text => addRow(text);
+  const deleteRow = index => () => setRows(
+    rows.filter((row, index) => index !== index) 
+  );
 
-    this.setState({
-      table
-    });
-  }
-
-  showRows = () => {
-    const {table} = this.state,
-      {deleteRow} = this;
-
-    return table.map( (row) => (
-      <tr className="row" key={row}>
-        <td className="hi">{row}</td>
-        <td>
-        <button className="buttons minus" data-row-id={row} onClick={ (e) =>  deleteRow(e)}>
-          <span>-</span>
-        </button>
-       </td>
-      </tr>
-     ));
-  }
-
-
-  deleteRow = (e) => {
-    const {table} = this.state,
-      btn = e.currentTarget,
-      rowId = parseInt(btn.getAttribute('data-row-id')),
-      changedTable = table.filter( (row) => row !== rowId);
-      console.log(rowId)
-    this.setState({table: changedTable});
-  }
-
-  render() {
-    return (
-     <table className="content">
-       <tbody>
-      <tr>
-        <td>
-        <button
-        className="plus buttons"
-        onClick={this.addRow}>
-         <span>+</span>
-        </button>
-       </td>
-       </tr>
-       {this.showRows()}      
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>
+           <button
+             className="plus buttons"
+             onClick={this.addRow}
+            >
+              <span>+</span>
+           </button>
+         </td>
+         </tr>
+         <tr>
+           {
+             rows.map(
+               (row, index) => (
+                 <Row onDelete={deleteRow(index)} text={row} /> 
+               )
+             )
+           }
+         </tr>
       </tbody>
-     </table> 
-    )
-  }
-}
+    </table>
+  ); 
+};
 
 export default MyTable;
